dcbf1e31c29da393dcc54c71d8837f92deca5c85
pd3v <paulo.develop@gmail.com>
2018-01-19 18:52:56 +0000

    redesigned synth class and add sequencer

--- synth.js
+++ synth.js
@@ -1,51 +1,46 @@
 class Synth {
   constructor(ac) {    
     this.osc = ac.createOscillator();
-    this.vector = ac.createOscillator();
-    this.oscGain = ac.createGain();
-    this.vectorGain = ac.createGain();
-    this.mixGain = ac.createGain();
+    this.adsrEnv = [0.01, 0, 1, 0.4];
   }
   
-  get frequency(){
-      return this.osc.frequency.value;
-  }
-  
-  set frequency(frequency) {
-      this.osc.frequency.value = frequency; 
-      this.vector.frequency.value = frequency; 
-  }
-  
-  get detune() {
-      return this.vector.frequency.value;
-  }
-  
-  set detune(frequency) {
-      this.vector.frequency.value = frequency; 
-  }
-  
-  get type(){
+  get type() {
       return this.osc.type;
   }
   
   set type(type) {
     this.osc.type = type;
-    this.vector.type = type;
   }
   
-  start() {
-    this.oscGain.gain.value = 0.5; 
-    this.vectorGain.gain.value = 0.5; 
-    this.mixGain.gain.value = 0.99;
-    this.osc.start();
-    this.vector.start();
+  get frequency() {
+      return this.osc.frequency.value;
   }
   
+  set frequency(frequency) {
+    this.osc.frequency.setValueAtTime(frequency, 0);
+  }
+  
+  get adsr() {
+    return this.adsrEnv;
+  }
+  
+  set adsr(value) {
+    this.adsrEnv = value;
+  }
+  
+  set frequency(frequency) {
+    this.osc.frequency.setValueAtTime(frequency, 0);
+  }
+
   connect(destination) {
-    this.osc.connect(this.oscGain);
-    this.vector.connect(this.vectorGain);
-    this.oscGain.connect(this.mixGain);
-    this.vectorGain.connect(this.mixGain);
-    this.mixGain.connect(destination);
+    this.osc.connect(destination);
+  }
+  
+  start(time=0) {
+    this.osc.start(time);
+  }
+  
+  stop(time=0) {
+    this.osc.stop(time);
   }
 }
